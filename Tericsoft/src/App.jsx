e394ec5ff1fe6e395387c2456cb0db205b811eb2
Shivam-Kumar9 <shivamkumar1019988@gmail.com>
2026-03-09 17:22:09 +0530

    fix: style error message in App component

--- Tericsoft/src/App.jsx
+++ Tericsoft/src/App.jsx
@@ -12,13 +12,12 @@ function App() {
       return <h1>Loading...</h1>
      }
      if(error){
-      return <h1>{error.message}</h1>}
+      return <h1 style={{color:'red'}} >{error.message}</h1>}
   return (<>
       <h1>Fetch Data from API</h1>
     <div className="container">
        {data && data.products.map( (item) =>
        <div key={item.id} className='card'>
-          
           <div className='fig'>
             <img src={item.thumbnail} alt={item.title} />
           </div>
@@ -28,7 +27,6 @@ function App() {
             <h3>Price: ${item.price}</h3>
              <div className='buy_btn'><button>Buy</button></div>
           </div>
-          
         </div> 
       )}
     </div></>
